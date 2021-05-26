4099b78155bcfd82ec641488d1749da10fca2f8d
Roni Wu <roniyy.wu@gmail.com>
2021-05-26 15:48:45 -0400

    removed newline

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import Form from './components/Form.js';
-
 import React, { useState, useEffect } from "react";
 import { Link, useHistory } from "react-router-dom";
 import { Nav, Navbar, NavItem } from "react-bootstrap";
